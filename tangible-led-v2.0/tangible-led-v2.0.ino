8566abbe3c0e84e9214014757b16a36f04478615
zhaonan <nan@hahdhah>
2017-03-07 13:37:44 -0500

    add acce mode

--- tangible-led-v2.0/tangible-led-v2.0.ino
+++ tangible-led-v2.0/tangible-led-v2.0.ino
@@ -2,9 +2,18 @@
 #include <Wire.h>
 #include "Adafruit_DRV2605.h"
 Adafruit_DRV2605 drv;
+#include <CapacitiveSensor.h> //capacitive sensor: send pin attached to the resistor
 
 //states
-bool totalState = LOW;
+int totalState = LOW;
+int acceState = LOW;
+
+//capacitive Sensor
+CapacitiveSensor   cs_4_2 = CapacitiveSensor(4, 2);       // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+int capaTouchHoldTime = 1000;
+int capaMinRead = 10000;
+long capacitiveStart = -1;
+long capacitiveEnd = -1;
 
 //the fsr sensor and states
 int fsrPin = A5;
@@ -25,9 +34,12 @@ int fsrPressDriveEffect = 80;
 
 
 
-int responseDelay = 50;
+int responseDelay = 30;
 
 void setup() {
+  //capacitive touch, calibration
+  cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+
   Serial.begin(9600);
   //begin the motor drive
   drv.begin();
@@ -37,8 +49,8 @@ void setup() {
 void loop() {
   //reading the fsr value
   fsrReading = analogRead(fsrPin);
-  Serial.print("fsrReading: ");
-  Serial.println(fsrReading);
+  //Serial.print("fsrReading: ");
+  //Serial.println(fsrReading);
   if (fsrReading <= fsrTouchStartPoint) {
     curFsrState = LOW;
   } else {
@@ -49,7 +61,7 @@ void loop() {
     //press down
     fsrDownTime = millis();
   }
-   if (curFsrState == LOW && prevFsrState == HIGH) {
+  if (curFsrState == LOW && prevFsrState == HIGH) {
     //press up
     fsrUpTime = millis();
   }
@@ -58,7 +70,7 @@ void loop() {
   //remind the click time is enough using motor drive.
   if (curFsrState == HIGH && (millis() - fsrDownTime) >= fsrHoldTime) {
     Serial.println("It's enough hold time.");
-    if (totalState == HIGH){
+    if (totalState == HIGH) {
       //only when the state is on, the click effect would be triggered
       motorLevel = fsrClickDriveEffect;
     }
@@ -69,15 +81,15 @@ void loop() {
     }
   }
 
-  //trigger the hold/press mode. 
+  //trigger the hold/press mode.
   if (curFsrState == LOW && prevFsrState == HIGH ) { //手抬起来
     if ((fsrUpTime - fsrDownTime) >= fsrHoldTime) {
       if ((fsrUpTime - fsrDownTime) >= fsrLongHoldTime) {
         Serial.println("trigger long hold | turn on/off");
         totalState = !totalState;
-        motorLevel = 0;      
+        motorLevel = 0;
       } else {
-        if(totalState == HIGH){
+        if (totalState == HIGH) {
           Serial.println("trigger clicked | change led mode");
         }
         motorLevel = 0;
@@ -87,12 +99,45 @@ void loop() {
     }
   }
   prevFsrState = curFsrState;
+
+  //sensing capacitive touch
+  if (totalState == HIGH ) {
+
+    //long capacitiveStart = millis();
+    long capacitiveRead =  cs_4_2.capacitiveSensor(30);
+    if (capacitiveRead >= capaMinRead) {
+      //capacitiveStart = millis();
+      acceState = HIGH;
+      Serial.print("capacitiveRead: ");
+      Serial.print(capacitiveRead);
+      Serial.println(" | start accelerometer mode.");
+    } else {
+      capacitiveEnd = millis();
+      acceState = LOW;
+    }
+
+//    //有问题啊
+//    if (millis() - capacitiveEnd > capaTouchHoldTime){
+//      Serial.println("test");
+//      if(capacitiveRead <capaMinRead){
+//        acceState = LOW;     
+//      }
+//    }
+  }
+
+  Serial.print("acceState: ");
+  Serial.println(acceState);
+  //capacitiveEnd = 0;
   
   Serial.print("totalState: ");
-  if (totalState == HIGH){Serial.println("on");}
-  if (totalState == LOW){Serial.println("off");}
+  if (totalState == HIGH) {
+    Serial.println("on");
+  }
+  if (totalState == LOW) {
+    Serial.println("off");
+  }
 
   //run the drive
   drv.setRealtimeValue(motorLevel);
-  delay(responseDelay);
+  //delay(responseDelay);
 }
--- testing_capacitiveTouch/testing_capacitiveTouch.ino
+++ testing_capacitiveTouch/testing_capacitiveTouch.ino
@@ -3,6 +3,9 @@
 //send pin attached to the resistor
 
 CapacitiveSensor   cs_4_2 = CapacitiveSensor(4, 2);       // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+int acceState = LOW;
+
+
 void setup() {
   cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
   Serial.begin(9600);
@@ -11,11 +14,19 @@ void setup() {
 void loop() {
 
   long start = millis();
-  long total1 =  cs_4_2.capacitiveSensor(30);
+  long total1 =  cs_4_2.capacitiveSensor(30); //original is 30
 
-  Serial.print(millis() - start);        // check on performance in milliseconds
-  Serial.print("\t");                    // tab character for debug windown spacing
-
-  Serial.println(total1);                  // print sensor output 1
+//  Serial.print(millis() - start);        // check on performance in milliseconds
+//  Serial.print("\t");                    // tab character for debug windown spacing
+//
+//  Serial.println(total1);                  // print sensor output 1
   delay(10);                             // arbitrary delay to limit data to serial port
+
+  if (total1 >= 10000){
+    acceState = HIGH;
+  }else{
+    acceState = LOW;
+  }
+
+  Serial.println(acceState);
 }
